8f202b4c72c137df3b808ae39c14d0d45b1877e8
Maurina_Davis <maurina.davis@gmail.com>
2019-10-02 17:04:02 -0600

    Maurina.vue component added

---
+++ src/developers/Maurina.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    <v-card
+      max-width="344"
+      class="mx-auto"
+    >
+      <v-card-title>Maurina Davis</v-card-title>
+      <v-card-text>Fall is here!</v-card-text>
+    </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
