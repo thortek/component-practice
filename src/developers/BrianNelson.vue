b04b4dee64ea18a283f123d93722abbb0aaa6882
brain <ladofmusi@outlook.com>
2019-10-02 15:47:16 -0600

    Brian vue app

---
+++ src/developers/BrianNelson.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+    <v-card-title>Brian Nelson</v-card-title>
+    
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    user: {
+      userName: Brian,
+      
+    }
+  })
+};
+</script>
+
+<style>
+.text-center {
+  text-align: center;
+}
+</style>
