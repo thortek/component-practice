0cb86385871c5c1b9f7790b427702588b9542d30
= <rdmanwill@gmail.com>
2019-10-07 15:11:53 -0600

    Added form element to RyanManwill.vue component

--- src/developers/RyanManwill.vue
+++ src/developers/RyanManwill.vue
@@ -1,7 +1,15 @@
 <template>
-    <v-app>
-        <h1>This is my addition</h1>
-    </v-app>
+    <v-row>
+        <v-col cols="12">
+            <h1>This is my addition</h1>
+            <v-form v-model="valid">
+                <v-text-field
+                    v-model="name"
+                    label="Your name"
+                ></v-text-field>
+            </v-form>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
