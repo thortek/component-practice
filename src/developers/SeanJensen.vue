150d15e25442ef13c96bbe00c82ef78f38163b47
seajensen <seajensen@gmail.com>
2019-10-07 15:09:14 -0600

    Added data for name

--- src/developers/SeanJensen.vue
+++ src/developers/SeanJensen.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
         <v-parallax src="https://res.cloudinary.com/twenty20/private_images/t_watermark-criss-cross-10/v1475125665000/photosp/e1d2ff68-b6a7-41da-a66b-bf3b670dee60/stock-photo-transportation-rain-raindrop-long-exposure-new-york-city-bokeh-vibrant-rainy-brooklyn-e1d2ff68-b6a7-41da-a66b-bf3b670dee60.jpg">
-          <h1>SEAN JENSEN</h1>
+          <h1>{{ name }}</h1>
         </v-parallax>
     </v-container>
 
@@ -10,8 +10,10 @@
 <script>
 
 export default {
-    
-}
+  data: () => ({
+    name: "SEAN JENSEN",
+  })
+};
 </script>
 
 <style scoped>
