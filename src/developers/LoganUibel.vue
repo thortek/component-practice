669ecadccd0820d84c13568e40e4a2c375922708
Logan Uibel <lbuibel@gmail.com>
2019-10-07 15:14:30 -0600

    Added form input

--- src/developers/LoganUibel.vue
+++ src/developers/LoganUibel.vue
@@ -1,10 +1,18 @@
 <template>
   <v-card
-    max-width="344"
     class="mx-auto"
   >
     <v-card-title>Logan Uibel</v-card-title>
     <v-card-text>Git Practice Assignment</v-card-text>
+    <v-card-action>
+    <v-text-field
+            p 2
+            v-model="firstname"
+            :rules="nameRules"
+            label="First"
+            required
+          ></v-text-field>
+    </v-card-action>
 
   </v-card>
 </template>
@@ -24,5 +32,7 @@ export default {
 
 
 <style scoped>
+
+}
 
 </style>
