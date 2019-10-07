88cf1e542dc6c19f49b7eb95fb01436bbde389bf
Peter <musicmandanceman@yahoo.com>
2019-10-07 15:24:33 -0600

    Removed adjustment due to upstream error

--- src/developers/PeterBeard.vue
+++ src/developers/PeterBeard.vue
@@ -2,10 +2,6 @@
   <v-card>
     <v-card-title>Peter Beard</v-card-title>
     <v-card-text>This is a component added by Peter Beard</v-card-text>
-    <v-card-action>
-      <label for="userMessage">Send type message for Peter:</label>
-      <textarea id="userMessage" v-model="userInput"></textarea>
-    </v-card-action>
   </v-card>
 </template>
 
@@ -15,8 +11,7 @@ export default {
     user: {
       userName: "Peter Beard",
       github: "Gibriil"
-    },
-    userInput: null
+    }
   })
 };
 </script>
