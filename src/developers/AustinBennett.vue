fcfb35900dd30f1d96e0644ba6e1bbb8bf296637
Austin Bennett <bboywake@gmail.com>
2019-10-09 15:24:46 -0600

    Added validation to slider and email field

--- src/developers/AustinBennett.vue
+++ src/developers/AustinBennett.vue
@@ -1,37 +1,50 @@
 <template>
 <v-row justify="space-around" class="row">
 	<v-col>
-		<v-slider
-			v-if="!range"
-			v-model="volume"
-			:min="min"
-			:max="max"
-			:disabled="disabled"
-			:readonly="readonly"
-			:vertical="vertical"
-			label="Volume"
-			class="slider"
-		></v-slider>
+		<v-form>
+			<v-slider
+				v-if="!range"
+				v-model="volume"
+				:min="min"
+				:max="max"
+				:disabled="disabled"
+				:readonly="readonly"
+				:vertical="vertical"
+				:rules="volumeRules"
+				label="Volume"
+				class="slider"
+			></v-slider>
+			<v-text-field v-model="email" label="Email Address" required :rules="emailRules"></v-text-field>
+		</v-form>
 	</v-col>
 </v-row>
 </template>
 
 <script>
-import Icon from 'vue-awesome/components/Icon'
-
+// import Icon from 'vue-awesome/components/Icon'
+const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 export default {
-    data () {
-      return {
-        volume: 10,
-        min: 0,
-        max: 100,
-        disabled: false,
-        readonly: false,
-        vertical: false,
-        range: false,
-      }
-    },
-  }
+	data: () => ({
+		volume: 10,
+		min: 0,
+		max: 100,
+		disabled: false,
+		readonly: false,
+		vertical: false,
+		range: false,
+		volumeRules: [
+			value => value >= 10 || 'Volume is too low'
+		],
+		valid: false,
+		email: '',
+		emailRules: [
+			value => emailRegex.test(value) || 'Valid email is required'
+		]
+	}),
+	methods: {
+
+	}
+}
 </script>
 
 <style scoped>
