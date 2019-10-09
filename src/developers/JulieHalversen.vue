edea13fcf6755539a19bfa7a345a0ceb72cbbf21
Julianne Halversen <juliannehalversen@gmail.com>
2019-10-09 14:30:34 -0600

    Added Form

--- src/developers/JulieHalversen.vue
+++ src/developers/JulieHalversen.vue
@@ -1,11 +1,49 @@
 <template>
+<div>
     <v-app>
         <h1>Julie Halversen</h1>
     </v-app>
+
+    <v-form v-model="valid">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-text-field
+              v-model="firstname"
+              label="First name"
+              required
+            ></v-text-field>
+          </v-col>
+  
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-text-field
+              v-model="lastname"
+              label="Last name"
+              required
+            ></v-text-field>
+          </v-col>
+  
+        </v-row>
+      </v-container>
+    </v-form>
+
+</div>
 </template>
 
 <script>
-
+export default {
+data: () => ({
+    valid: false,
+    firstname: '',
+    lastname: '',
+  })
+}
 </script>
 
 <style scoped>
